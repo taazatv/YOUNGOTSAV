cde588ec25545c2d291a82b619042b8303084f4e
Taazatv2 <taazatv2@gmail.com>
2025-03-31 13:41:28 +0530

    links add on gallery section

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -12,24 +12,24 @@ export default function Gallery() {
         <HoverImage
           className="h-full md:w-2/4"
           src={"/gallery/1.png"}
-          link="/video.mp4"
+          link=" https://www.youtube.com/watch?v=4B225E6CS5I"
         />
         <div className="flex flex-col md:w-1/4">
           <HoverImage
             src={"/gallery/2.png"}
             className="aspect-square w-full"
-            link="https://youtu.be/4B225E6CS5I?si=RRRX5v2lbsY1C8gJ"
+            link=" https://www.instagram.com/p/DG3OupJzBPE/"
           />
           <HoverImage
             src={"/gallery/3.png"}
             className="aspect-square w-full"
-            link="https://www.instagram.com/reel/DG7PthdtI98/?igsh=MW02ZGV0c2VlYWM4cA=="
+            link="https://www.instagram.com/p/DG7PthdtI98/"
           />
         </div>
         <HoverImage
           src={"/gallery/4.png"}
           className="aspect-[1/2] md:w-1/4"
-          link="#"
+          link=" https://www.instagram.com/p/DG3fbcgSOUJ/"
         />
       </div>
       <div className="flex w-full max-md:flex-col">
@@ -37,18 +37,18 @@ export default function Gallery() {
           <HoverImage
             src={"/gallery/5.png"}
             className="aspect-[3/2] w-full"
-            link="#"
+            link=" https://www.youtube.com/watch?v=hPlxb76XDdw"
           />
           <HoverImage
             src={"/gallery/6.png"}
             className="aspect-[3/2] w-full"
-            link="#"
+            link="https://www.instagram.com/p/DGyJ7JbSO1S/"
           />
         </div>
         <HoverImage
           className="aspect-[5/4] md:w-3/5"
           src={"/gallery/7.png"}
-          link="#"
+          link="https://www.instagram.com/p/DGxB1tpz54C/"
         />
       </div>
     </section>
