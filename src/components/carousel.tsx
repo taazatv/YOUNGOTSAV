96ccb4d5755b1bfec81705b1ec50edf44427774a
amitverma8835 <amitkv93047@gmail.com>
2026-02-22 23:27:35 +0530

    video added

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,19 +1,28 @@
-import video from "@/assets/video.mp4";
+import topVideo from "@/assets/youngotsav.mp4";
+import mainVideo from "@/assets/video.mp4";
 
 export default function Carousel() {
   return (
     <section
       id="carousel"
-      // className="bg-secondary flex w-screen flex-col items-center gap-8 bg-[url(https://picsum.photos/1600/900)] p-8 py-44 bg-blend-multiply md:py-56"
-      className="relative flex aspect-video h-auto w-screen flex-col items-center gap-8 overflow-hidden"
+      className="relative flex w-screen flex-col items-center overflow-hidden"
     >
       <video
         autoPlay
         muted
         loop
         playsInline
-        src={video}
-        className="bg-secondary absolute top-0 -z-20 h-full w-full bg-blend-multiply"
+        src={topVideo}
+        className="w-full h-[30vh] object-cover"
+      />
+
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        src={mainVideo}
+        className="w-full h-[45vh] object-cover"
       />
     </section>
   );
